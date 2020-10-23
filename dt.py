0cb7ea18966bce39b5b2a68c316e91c4da407b85
Gary Gao <gykawesome@163.com>
2020-10-24 00:39:31 +0100

    enricher the skeleton, added entropy calculater

--- dt.py
+++ dt.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 
 # util imports (MUST REMOVE when submitted)
@@ -16,6 +16,12 @@ def create_csv_data_from_txt(inputfile):
 def decision_tree_learning(traning_dataset, depth):
     pass
 
+def calc_entropy(data):
+    result = 0
+    for i in data:
+        result -= np.log2(data[i])
+    return result
+
 
 if __name__ == '__main__':
     inputfile = './wifi_db/clean_dataset.txt'
@@ -24,9 +30,32 @@ if __name__ == '__main__':
 
 # decision_tree format: python dictionary: {'attribute', 'value', 'left', 'right', 'leaf'}
 # split rule: trial0: split by room numbers.
-    training_dataset = numpy.loadtxt(inputfile)
+    training_dataset = np.loadtxt(inputfile)
+    depth = 0
 
+# do statistic for the sample size of different rooms
+    rooms = dict()
+    for rowi in training_dataset:
+        room_id = rowi[-1]
+        if room_id in rooms:
+            rooms[room_id] = rooms[room_id] + 1
+        else:
+            rooms[room_id] = 1
+# turns out that there are 500 samples for each room.
+
+# then calculate the proportion of samples in each room
+    total_num = len(training_dataset)
+    room_ratio = dict()
+    for i in range(len(rooms)):
+        curr_num = rooms[i+1]
+        room_ratio['room'+ str(i+1)] = curr_num/total_num
+
+# after getting the ratio, we can calculate the entropy:
+    entropy_all = calc_entropy(room_ratio)
+    print(entropy_all)
+
+# TODO: then loop through the remainder to get Information Gain:
+
+
+# TODO: complete this function after
     decision_tree_learning(training_dataset, depth)
-
-
-    print('initial commit.')
