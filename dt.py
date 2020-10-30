770d7823f8aeb10d6c49f24ba95c2f2558ca841f
Gary Gao <gykawesome@163.com>
2020-10-30 11:44:43 +0000

    fix bugs, now iterates through every element in the dataset

--- dt.py
+++ dt.py
@@ -60,44 +60,50 @@ def find_split(training_dataset):
     cur_split_point = 0
     left_split = []
     right_split = []
-    for split_signal in range(-100, 0):
-        # List of numbers of data for rooms 1-4 with wifi signal larger than split signal
-        larger_data_num = [0] * 4
-        # List of numbers of data for rooms 1-4 with wifi signal smaller than split signal
-        smaller_data_num = [0] * 4
 
-        for rowi in training_dataset:
-            roomi = int(rowi[-1])
-            if rowi[roomi - 1] > split_signal:
-                larger_data_num[roomi - 1] += 1
-                left_split.append(rowi)
-            else:
-                smaller_data_num[roomi - 1] += 1
-                right_split.append(rowi)
+    for wifi_i in range(0, 7):
+        for split_signal in range(-100, 0):
+            left_split.clear()
+            right_split.clear()
+            # List of numbers of data for rooms 1-4 with wifi signal larger than split signal
+            larger_data_num = [0] * 4
+            # List of numbers of data for rooms 1-4 with wifi signal smaller than split signal
+            smaller_data_num = [0] * 4
 
-        larger_data_total = sum(larger_data_num)
-        smaller_data_total = sum(smaller_data_num)
-        all_data_total = larger_data_total + smaller_data_total
-        larger_data_ratio = []
-        smaller_data_ratio = []
-        for larger_data in larger_data_num:
-            if larger_data_total != 0:
-                larger_data_ratio.append(larger_data / larger_data_total)
-        for smaller_data in smaller_data_num:
-            if smaller_data_total != 0:
-                smaller_data_ratio.append(smaller_data / smaller_data_total)
-        larger_entropy = calc_entropy_list(larger_data_ratio)
-        smaller_entropy = calc_entropy_list(smaller_data_ratio)
-        remainder = larger_entropy * larger_data_total / all_data_total + smaller_entropy * smaller_data_total / all_data_total
-        ig = current_entropy - remainder
-        if ig > cur_max_ig:
-            cur_max_ig = ig
-            cur_split_point = split_signal
-    print(cur_max_ig)
-    print(cur_split_point)
+            for rowi in training_dataset:
+                roomi = int(rowi[-1])
+                signal_roomi = rowi[wifi_i]
+                if signal_roomi > split_signal:
+                    larger_data_num[roomi - 1] += 1
+                    left_split.append(rowi)
+                else:
+                    smaller_data_num[roomi - 1] += 1
+                    right_split.append(rowi)
 
-    return {'left_split': left_split, 'right_split': right_split}, cur_split_point
+            larger_data_total = sum(larger_data_num)
+            smaller_data_total = sum(smaller_data_num)
+            all_data_total = larger_data_total + smaller_data_total
+            larger_data_ratio = []
+            smaller_data_ratio = []
+            for larger_data in larger_data_num:
+                if larger_data_total != 0:
+                    larger_data_ratio.append(larger_data / larger_data_total)
+            for smaller_data in smaller_data_num:
+                if smaller_data_total != 0:
+                    smaller_data_ratio.append(smaller_data / smaller_data_total)
+            larger_entropy = calc_entropy_list(larger_data_ratio)
+            smaller_entropy = calc_entropy_list(smaller_data_ratio)
+            remainder = larger_entropy * larger_data_total / all_data_total + smaller_entropy * smaller_data_total / all_data_total
+            ig = current_entropy - remainder
+            if ig > cur_max_ig:
+                cur_max_ig = ig
+                cur_split_point = split_signal
+                left_branch = list(left_split)
+                right_branch = list(right_split)
+                curr_wifi_number = wifi_i
 
+        print(cur_split_point)
+        return {'left_split': left_branch, 'right_split': right_branch}, cur_split_point, curr_wifi_number+1
 
 # TODO: then loop through the remainder to get Information Gain:
 
@@ -112,14 +118,16 @@ def decision_tree_learning(training_dataset, depth):
 
     if isUniTag:
         # COMMENT: return a pair of node and depth
-        return ({'attribute': 'leaf', 'value': 0, 'left': None, 'right': None, 'leaf': True}, depth)
+        return ({'attribute': 'leaf', 'value': tags[0], 'left': None, 'right': None, 'leaf': True}, depth)
     else:
         split = find_split(training_dataset)
+        signal_strenth = split[1]
+        wifi_number = split[2]
         l_dataset = split[0]['left_split']
         r_dataset = split[0]['right_split']
-        (l_branch, l_depth), split_point = decision_tree_learning(l_dataset, depth + 1)
-        (r_branch, r_depth), split_point = decision_tree_learning(r_dataset, depth + 1)
-        node = {'attribute': 'wifi_signal > ', 'value': split[1], 'left': l_branch, 'right': r_branch, 'leaf': False}
+        (l_branch, l_depth) = decision_tree_learning(l_dataset, depth + 1)
+        (r_branch, r_depth) = decision_tree_learning(r_dataset, depth + 1)
+        node = {'attribute': 'wifi' + str(wifi_number) + '_signal > ', 'value': signal_strenth, 'left': l_branch, 'right': r_branch, 'leaf': False}
         return (node, max(l_depth, r_depth))
 
 
