d6b9f383bc4701f688fe90994d363b461b0270c6
Gary Gao <gykawesome@163.com>
2020-10-30 09:43:14 +0000

    modified decision_tree_learning to return with a decision tree

--- dt.py
+++ dt.py
@@ -96,61 +96,13 @@ def find_split(training_dataset):
     print(cur_max_ig)
     print(cur_split_point)
 
-
-
-
-        # split = find_split(training_dataset)
-        # l_dataset = split['left_split']
-        # r_dataset = split['right_split']
-
-
-
-
-
-        # for split_signal in range(-100, 0):
-        #     number_positive = 0
-        #     number_negative = 0
-        #     roomi_wifii_ratios = dict()
-        #     roomi_wifii_ratios_negative = dict()
-        #     for wifi_number in range(1, 8):
-        #         for rowi in training_dataset:
-        #             if rowi[wifi_number - 1] > split_signal and rowi[-1] == roomi:
-        #                 number_positive += 1
-        #             elif rowi[wifi_number - 1] <= split_signal and rowi[-1] == roomi:
-        #                 number_negative += 1
-        #         roomi_wifii_ratios['room' + str(roomi) + '_wifi' + str(wifi_number)] = number_positive / number_target_room
-        #         roomi_wifii_ratios['room' + str(roomi) + '_wifi' + str(wifi_number) + '_others'] = (number_target_room - number_positive) / number_target_room
-        #         roomi_wifii_ratios_negative['room' + str(roomi) + '_wifi' + str(wifi_number)] = number_negative / number_target_room
-        #         roomi_wifii_ratios_negative['room' + str(roomi) + '_wifi' + str(wifi_number) + '_others'] = (number_target_room - number_negative) / number_target_room
-        #
-        #         print(roomi_wifii_ratios)
-        #         print(roomi_wifii_ratios_negative)
-        #         entropy_roomi_wifii = calc_entropy(roomi_wifii_ratios)
-        #         entropy_roomi_wifii_negative = calc_entropy(roomi_wifii_ratios_negative)
-        #
-        #         # TODO: bug exists here: the IG obtained is incorrect.
-        #         IG = entropy_i - ((number_positive/number_total) * entropy_roomi_wifii + (number_negative/number_total) * entropy_roomi_wifii_negative)
-    #
-    #
-    #             number_positive = 0
-    #             number_negative = 0
-    #             roomi_wifii_ratios.clear()
-    #             roomi_wifii_ratios_negative.clear()
-    #
-    #             # print(IG)
-    #             split_list.append(IG)
-    #             split_dict[IG] = split_signal, wifi_number
-    #     IG_max = np.max(split_list)
-    #     split_pivot = split_dict[IG_max]
-    #     print(split_pivot) # ideally this should give a split pivot signal strength.
-    #
-    return {'left_split': left_split, 'right_split': right_split}
+    return {'left_split': left_split, 'right_split': right_split}, cur_split_point
 
 
 # TODO: then loop through the remainder to get Information Gain:
 
 
-def decision_tree_learning(training_dataset):
+def decision_tree_learning(training_dataset, depth):
     tags = []
     for rowi in training_dataset:
         tags.append(rowi[-1])
@@ -160,33 +112,15 @@ def decision_tree_learning(training_dataset):
 
     if isUniTag:
         # COMMENT: return a pair of node and depth
-        return
-    split = find_split(training_dataset)
-    l_dataset = split['left_split']
-    r_dataset = split['right_split']
-    decision_tree_learning(l_dataset)
-    decision_tree_learning(r_dataset)
-
-
-# def decision_tree_learning(traning_dataset, depth):
-#     tags = []
-#     for rowi in training_dataset:
-#         tags.append(rowi[-1])
-#
-#     # check if all tags are the same
-#     isUniTag = tags.count(tags[0]) == len(tags)
-#
-#     if isUniTag:
-#         # COMMENT: return a pair of node and depth
-#         return ({'attribute': 'leaf', 'value': 0, 'left': None, 'right': None, 'leaf': True}, depth)
-#     else:
-#         split = find_split(training_dataset)
-#         l_dataset = split['left_split']
-#         r_dataset = split['right_split']
-#         node = {'attribute': split['wifi_number'], 'value': 0, 'left': None, 'right': None, 'leaf': False}
-#         (l_branch, l_depth) = decision_tree_learning(l_dataset, depth + 1)
-#         (r_branch, r_depth) = decision_tree_learning(r_dataset, depth + 1)
-#         return (node, max(l_depth, r_depth))
+        return ({'attribute': 'leaf', 'value': 0, 'left': None, 'right': None, 'leaf': True}, depth)
+    else:
+        split = find_split(training_dataset)
+        l_dataset = split[0]['left_split']
+        r_dataset = split[0]['right_split']
+        (l_branch, l_depth), split_point = decision_tree_learning(l_dataset, depth + 1)
+        (r_branch, r_depth), split_point = decision_tree_learning(r_dataset, depth + 1)
+        node = {'attribute': 'wifi_signal > ', 'value': split[1], 'left': l_branch, 'right': r_branch, 'leaf': False}
+        return (node, max(l_depth, r_depth))
 
 
 if __name__ == '__main__':
@@ -199,4 +133,4 @@ if __name__ == '__main__':
     depth = 0
 
     # TODO: complete this function after
-    decision_tree_learning(training_dataset)
+    decision_tree_learning(training_dataset, depth)
