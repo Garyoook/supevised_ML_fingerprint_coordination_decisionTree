445f2f34dbce8b8070c8bb60f1a3b05e85caa4d6
Gary Gao <gykawesome@163.com>
2020-11-02 16:37:11 +0000

    fixed bug in shuffling the training data

--- dt.py
+++ dt.py
@@ -1,17 +1,5 @@
 import numpy as np
 import random
-from matplotlib import pyplot as plt
-
-# util imports (MUST REMOVE when submitted)
-import pandas as pd
-
-
-def create_csv_data_from_txt(inputfile):
-    outputfile = './wifi_db/clean_dataset.csv'
-    print('generating csv...')
-    data = pd.read_csv(inputfile, sep='\t',
-                       names=['rssi1', 'rssi2', 'rssi3', 'rssi4', 'rssi5', 'rssi6', 'rssi7', 'room'])
-    data.to_csv(outputfile, header=['rssi1', 'rssi2', 'rssi3', 'rssi4', 'rssi5', 'rssi6', 'rssi7', 'room'], index=0)
 
 
 def calc_entropy(data):
@@ -171,10 +159,10 @@ if __name__ == '__main__':
     # COMMENT: decision_tree format: python dictionary: {'attribute', 'value', 'left', 'right', 'leaf'}
     # COMMENT: split rule: trial0: split by room numbers.
     training_dataset = np.loadtxt(inputfile)
-    db_list = random.shuffle(training_dataset)
+    random.shuffle(training_dataset)
     depth = 0
 
     # TODO: complete this function after
-    (d_tree, depth) = decision_tree_learning(db_list, depth)
+    (d_tree, depth) = decision_tree_learning(training_dataset, depth)
 
     print(tree_toString(d_tree, depth))
