9638ab5741684c417c009bdbeaf847b2588f54ff
HY <yan@dyn3175-189.wlan.ic.ac.uk>
2020-11-06 02:28:48 +0000

    New Pruning Algorithm

--- pruning.py
+++ pruning.py
@@ -11,48 +11,48 @@ from evaluate import evaluate, get_confusion_matrix, get_recall, get_precision, 
 from visualise_dtree import visualise_decision_tree, get_tree_depth
 
 
-def prune(test_data, d_tree):
-    prune_helper(test_data, d_tree, d_tree)
-
-
-def prune_helper(test_data, node, d_tree):
-    if not node["left"]["leaf"]:
-        prune_helper(test_data, node["left"], d_tree)
-    if not node["right"]["leaf"]:
-        prune_helper(test_data, node["right"], d_tree)
-    if node["left"]["leaf"] and node["right"]["leaf"]:
-        accuracy = evaluate(test_data, d_tree)
-        curr_node = node.copy()
-        node.update(curr_node["left"].copy())
-        if evaluate(test_data, d_tree) < accuracy:
-            node.update(curr_node.copy())
-        else:
-            left_node = node.copy()
-            node.update(curr_node["right"].copy())
-            if evaluate(test_data, d_tree) < accuracy:
-                node.update(left_node.copy())
-
-
 # def prune(test_data, d_tree):
-#     layers = get_layers(d_tree)
-#     accuracy = evaluate(test_data, d_tree)
-#     while layers:
-#         layer = layers.pop()
-#         for node in layer:
-#             if node["left"]["leaf"] and node["right"]["leaf"]:
-#                 curr_node = node.copy()
-#                 node.update(curr_node["left"].copy())
-#                 if evaluate(test_data, d_tree) < accuracy:
-#                     node.update(curr_node.copy())
-#                     node.update(curr_node["right"].copy())
-#                     if evaluate(test_data, d_tree) < accuracy:
-#                         node.update(curr_node.copy())
-#                 else:
-#                     left_node = node.copy()
-#                     node.update(curr_node["right"].copy())
-#                     if evaluate(test_data, d_tree) < accuracy:
-#                         node.update(left_node.copy())
-#     return d_tree
+#     prune_helper(test_data, d_tree, d_tree)
+#
+#
+# def prune_helper(test_data, node, d_tree):
+#     if not node["left"]["leaf"]:
+#         prune_helper(test_data, node["left"], d_tree)
+#     if not node["right"]["leaf"]:
+#         prune_helper(test_data, node["right"], d_tree)
+#     if node["left"]["leaf"] and node["right"]["leaf"]:
+#         accuracy = evaluate(test_data, d_tree)
+#         curr_node = node.copy()
+#         node.update(curr_node["left"].copy())
+#         if evaluate(test_data, d_tree) < accuracy:
+#             node.update(curr_node.copy())
+#         else:
+#             left_node = node.copy()
+#             node.update(curr_node["right"].copy())
+#             if evaluate(test_data, d_tree) < accuracy:
+#                 node.update(left_node.copy())
+
+
+def prune(test_data, d_tree):
+    layers = get_layers(d_tree)
+    accuracy = evaluate(test_data, d_tree)
+    while layers:
+        layer = layers.pop()
+        for node in layer:
+            if node["left"]["leaf"] and node["right"]["leaf"]:
+                curr_node = node.copy()
+                node.update(curr_node["left"].copy())
+                if evaluate(test_data, d_tree) < accuracy:
+                    node.update(curr_node.copy())
+                    node.update(curr_node["right"].copy())
+                    if evaluate(test_data, d_tree) < accuracy:
+                        node.update(curr_node.copy())
+                else:
+                    left_node = node.copy()
+                    node.update(curr_node["right"].copy())
+                    if evaluate(test_data, d_tree) < accuracy:
+                        node.update(left_node.copy())
+    return d_tree
 
 
 def get_layers(d_tree):
@@ -75,109 +75,125 @@ def get_layers(d_tree):
 
 
 def cross_validation(all_db_list):
+    # set up heading for evaluation result table
     header_list = ["index", "accuracy", "precision", "recall", "f1",
                    "maximal depth before pruning", "maximal depth after pruning"]
-    # set up heading for evaluation result table
-    class_list = ["room1", "room2", "room3", "room4"]  # set up heading for the confusion matrix
+    # set up heading for the confusion matrix
+    class_list = ["room1", "room2", "room3", "room4"]
+    # set up text-table for average results of all metrics
     macro_table = Texttable()
     macro_table.header(header_list)
 
-    d_tree_max_accuracy = dict()
+    # total accuracy, precision, recall, f1 scores and confusion matrix for all 10 folds of validation
+    total_accuracy = [0] * CLASS_NUM
+    total_precision = [0] * CLASS_NUM
+    total_recall = [0] * CLASS_NUM
+    total_f1 = [0] * CLASS_NUM
+    total_matrix = np.zeros((CLASS_NUM, CLASS_NUM))
 
-    for roomi in range(CLASS_NUM):  # validate for each class (room)
-        max_accuracy = 0
-        # total accuracy, precision, recall, f1 scores and confusion matrix for all 10 folds of validation
-        total_accuracy = 0
-        total_precision = 0
-        total_recall = 0
-        total_f1 = 0
-        total_matrix = np.zeros((CLASS_NUM, CLASS_NUM))
-        # maximum depth of all decision trees generated
-        max_depth = 0
-        max_depth_after_pruning = 0
-        # calculate step size
-        db_size = len(all_db_list)
-        step = db_size // FOLD_NUM
-        training_validation_db_size = 0
-        # initialise list for result output
-        output = [header_list]
+    # maximum depth of all decision trees generated
+    max_depth = 0
+    max_depth_after_pruning = 0
 
-        for start in range(0, db_size, step):
-            # separate data into (training + validation) data and test data
-            end = start + step
-            test_db, training_validation_db = separate_data(all_db_list, start, end, db_size)
+    # calculate step size
+    db_size = len(all_db_list)
+    step = db_size // FOLD_NUM
 
-            training_validation_db_size = len(training_validation_db)
+    # initialise 4 charts for result output
+    metric_charts_display = []
+    for i in range(CLASS_NUM):
+        t = Texttable()
+        t.add_row(header_list)
+        metric_charts_display.append(t)
 
-            for nested_start in range(0, training_validation_db_size, step):
-                # separate (training + validation) data into training data and validation data
-                nested_end = nested_start + step
-                validation_db, training_db = separate_data(training_validation_db, nested_start, nested_end,
-                                                           training_validation_db_size)
+    for start in range(0, db_size, step):
+        # separate data into (training + validation) data and test data
+        end = start + step
+        test_db, training_validation_db = separate_data(all_db_list, start, end, db_size)
 
-                # training
-                d_tree, depth = dt.decision_tree_learning(training_db, 0)
+        training_validation_db_size = len(training_validation_db)
 
-                # update maximum depth before pruning
-                if depth > max_depth:
-                    max_depth = depth
+        for nested_start in range(0, training_validation_db_size, step):
+            # separate (training + validation) data into training data and validation data
+            nested_end = nested_start + step
+            validation_db, training_db = separate_data(training_validation_db, nested_start, nested_end,
+                                                       training_validation_db_size)
 
-                # visualise_decision_tree(d_tree, depth, 'tree_images/decision_tree_no_prune.png')
-                # this and below visualisation are used for validating prune()
+            # training
+            d_tree, depth = dt.decision_tree_learning(training_db, 0)
 
-                # pruning
-                prune(validation_db, d_tree)
-                depth_after_pruing = get_tree_depth(d_tree)  # update depth after pruning
+            # update maximum depth before pruning
+            if depth > max_depth:
+                max_depth = depth
 
-                # visualise_decision_tree(d_tree, depth, 'tree_images/decision_tree_with_prune.png')
-                # update maximum depth after pruning
-                if depth_after_pruing > max_depth_after_pruning:
-                    max_depth_after_pruning = depth_after_pruing
+            # pruning
+            prune(validation_db, d_tree)
+            depth_after_pruing = get_tree_depth(d_tree)  # update depth after pruning
 
+            # update maximum depth after pruning
+            if depth_after_pruing > max_depth_after_pruning:
+                max_depth_after_pruning = depth_after_pruing
+
+            # get confusion matrix
+            confusion_matrix = get_confusion_matrix(test_db, d_tree)
+            total_matrix = np.array(confusion_matrix) + np.array(total_matrix)
+
+            # display confusion matrix
+            matrix_display = Texttable()
+            matrix_display.header(class_list)
+            for i in range(CLASS_NUM):
+                matrix_display.add_row(confusion_matrix[i])
+            fold_num = np.int(np.ceil(start / step)) + 1
+            inner_fold_num = np.int(np.ceil(nested_start / step)) + 1
+            print('Confusion matrix of fold ' + str(fold_num) + '-' + str(inner_fold_num) + ' is: ')
+            print(matrix_display.draw())  # print average confusion matrix
+            print()
+
+            for roomi in range(CLASS_NUM):  # validate for each class (room)
                 # calculate metrics
-                confusion_matrix = get_confusion_matrix(test_db, d_tree)
                 precision = get_precision(roomi, confusion_matrix)
                 recall = get_recall(roomi, confusion_matrix)
                 f1 = get_f1(roomi, confusion_matrix)
                 accuracy = get_accuracy(roomi, confusion_matrix)
-                total_precision += precision
-                total_recall += recall
-                total_f1 += f1
-                total_accuracy += accuracy
-                total_matrix = np.array(confusion_matrix) + np.array(total_matrix)
+                total_precision[roomi] += precision
+                total_recall[roomi] += recall
+                total_f1[roomi] += f1
+                total_accuracy[roomi] += accuracy
 
-                if accuracy > max_accuracy:
-                    max_accuracy = accuracy
-                    d_tree_max_accuracy[roomi - 1] = (d_tree, depth)
-
-                # print results
-                col = [str(start // step + 1), str(accuracy), str(precision), str(recall), str(f1),
+                # add result of each fold to the text-table of each room
+                col = [str(fold_num) + '-' + str(inner_fold_num), str(accuracy), str(precision), str(recall), str(f1),
                        str(depth), str(depth_after_pruing)]
-                output.append(col)
-        t = Texttable()
-        t.add_rows(output)
+                metric_charts_display[roomi].add_row(col)
+
+    total_fold_num = FOLD_NUM * (FOLD_NUM - 1)
+
+    for roomi in range(CLASS_NUM):  # display results for each room
         print('Evaluation result for room' + str(roomi + 1) + ' is: ')
-        stats_denom = np.ceil((training_validation_db_size / step))
-        average_result = ["average of room" + str(roomi + 1), str(total_accuracy / (stats_denom * FOLD_NUM)),
-                          str(total_precision / (stats_denom * FOLD_NUM)),
-                          str(total_recall / (stats_denom * FOLD_NUM)), str(total_f1 / (stats_denom * FOLD_NUM)),
+        average_result = ["average of room" + str(roomi + 1), str(total_accuracy[roomi] / total_fold_num),
+                          str(total_precision[roomi] / total_fold_num),
+                          str(total_recall[roomi] / total_fold_num), str(total_f1[roomi] / total_fold_num),
                           str(max_depth) + ' (Note: depths are maximal rather than avg value)',
                           str(max_depth_after_pruning)]
         macro_table.add_row(average_result)
-        t.add_row(average_result)
-        print(t.draw())  # print "index", "accuracy", "precision", "recall", "f1" of each fold
-        average_matrix = np.array(total_matrix) / (stats_denom * FOLD_NUM)
-        m = Texttable()
-        m.header(class_list)
-        for i in range(CLASS_NUM):
-            m.add_row(average_matrix[i])
-        print('average confusion matrix for room ' + str(roomi + 1) + ' is: ')
-        print(m.draw())  # print average confusion matrix
-    print(macro_table.draw())
+        metric_charts_display[roomi].add_row(average_result)
+        # print "index", "accuracy", "precision", "recall", "f1" of each fold for each room
+        print(metric_charts_display[roomi].draw())
+        print()
 
-    # for key in d_tree_max_accuracy:
-    #     visualise_decision_tree(d_tree_max_accuracy[key][0], d_tree_max_accuracy[key][1],
-    #                             'tree_images/decision_tree_' + str(key) + '.png')
+    # display confusion matrix
+    average_matrix = np.array(total_matrix) / (total_fold_num)
+    matrix_display = Texttable()
+    matrix_display.header(class_list)
+    for i in range(CLASS_NUM):
+        matrix_display.add_row(average_matrix[i])
+    print('Average confusion matrix is: ')
+    print(matrix_display.draw())  # print average confusion matrix
+    print()
+
+    # display average results in all folds for each room
+    print('Average metrics for each room is:')
+    print(macro_table.draw())
+    print()
 
 
 if __name__ == '__main__':
